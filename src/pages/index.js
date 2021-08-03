36f2bf6a5d9f67b8fc505dbef6bdbd68127fa297
Kevin Thizy <kevin.thizy@intech.lu>
2021-08-03 12:44:46 +0200

    ✨() Setup categories

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/onchainid/intro">
+            to="/docs/concepts/intro">
             Discover more
           </Link>
           <Link
